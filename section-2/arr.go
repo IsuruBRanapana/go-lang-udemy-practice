21e5dde1b027bcadd4c411bb8ebb1d246f3f5886
Isuru B. Ranapana <raaisurub.ranapana@gmail.com>
2020-08-11 20:49:19 +0530

    learn pointers

--- section-2/arr.go
+++ section-2/arr.go
@@ -11,6 +11,18 @@ func todo(){
 	fmt.Println(arr2)
 }
 
+//pointers
+func swap(m1,m2 *int){
+	var temp int
+	temp = *m2
+	*m2 = *m1
+	*m1=temp
+}
+
 func main(){
 	todo()
+	m1,m2 := 2,3
+	fmt.Println(m1,m2)
+	swap(&m1,&m2)
+	fmt.Println(m1,m2)
 }
