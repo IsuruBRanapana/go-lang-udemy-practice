2ca27d2c632e8417bd3670acf493a9706be5c768
Isuru B. Ranapana <raaisurub.ranapana@gmail.com>
2020-08-19 20:41:19 +0530

    MVC Start with API

--- Section-4/todo-api/main.go
+++ Section-4/todo-api/main.go
@@ -1,21 +1,11 @@
 package main
 
 import (
-	"encoding/json"
 	"net/http"
-	"udemy/Section-4/todo-api/structs"
+	"udemy/Section-4/todo-api/controller"
 )
 
 func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == http.MethodGet {
-			data := structs.Response{
-				Code: http.StatusOK,
-				Body: "pong",
-			}
-			json.NewEncoder(w).Encode(data)
-		}
-	})
+	mux := controller.Register()
 	http.ListenAndServe(":3000", mux)
 }
