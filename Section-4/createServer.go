7955baf76b9a5b49c2fc04b3d82d68c6b6e72aef
Isuru B. Ranapana <raaisurub.ranapana@gmail.com>
2020-08-19 20:23:05 +0530

    Todo API Start

--- Section-4/createServer.go
+++ Section-4/createServer.go
@@ -8,9 +8,16 @@ import (
 func main() {
 	mux := http.NewServeMux()
 	//end point
-	mux.HandleFunc("/getgoing", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("/getgoing/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("receive request")
+		fmt.Println(r.Method)
 		w.Write([]byte("Hello World"))
+	})
+
+	mux.HandleFunc("/getgoing/go", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("receive request")
+		fmt.Println(r.Method)
+		w.Write([]byte("Hello World go"))
 	})
 	http.ListenAndServe("localhost:3000", mux)
 }
---
+++ Section-4/todo-api/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"udemy/Section-4/todo-api/structs"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodGet {
+			data := structs.Response{
+				Code: http.StatusOK,
+				Body: "pong",
+			}
+			json.NewEncoder(w).Encode(data)
+		}
+	})
+	http.ListenAndServe(":3000", mux)
+}
