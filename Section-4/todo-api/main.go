6a8bc5c3460a4326af905b82db27e31404f8c06d
Isuru B. Ranapana <raaisurub.ranapana@gmail.com>
2020-08-20 19:42:29 +0530

    Stablish DB Connection

--- Section-4/todo-api/main.go
+++ Section-4/todo-api/main.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"log"
 	"net/http"
 	"udemy/Section-4/todo-api/controller"
+	"udemy/Section-4/todo-api/model"
 )
 
 func main() {
 	mux := controller.Register()
-	http.ListenAndServe(":3000", mux)
+	db := model.Connect()
+	defer db.Close()
+	fmt.Println("Serving....")
+	log.Fatal(http.ListenAndServe(":3000", mux))
 }
