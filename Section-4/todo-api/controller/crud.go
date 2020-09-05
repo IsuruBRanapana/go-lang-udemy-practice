aae281901f7f1de1e6a23d1b1af4a6aa1197c2f9
Isuru B. Ranapana <raaisurub.ranapana@gmail.com>
2020-09-05 22:59:58 +0530

    Delete is done

--- Section-4/todo-api/controller/crud.go
+++ Section-4/todo-api/controller/crud.go
@@ -21,13 +21,32 @@ func create() http.HandlerFunc {
 			w.WriteHeader(http.StatusCreated)
 			json.NewEncoder(w).Encode(data)
 		} else if r.Method == http.MethodGet {
-			data, err := model.ReadAll()
+			///these are for get all///
+			//data, err := model.ReadAll()
+			//if err != nil {
+			//	w.Write([]byte(err.Error()))
+			//}
+			//w.WriteHeader(http.StatusOK)
+			//json.NewEncoder(w).Encode(data)
+
+			///these are for selection
+			name := r.URL.Query().Get("name")
+			data, err := model.ReadByName(name)
 			if err != nil {
 				w.Write([]byte(err.Error()))
 			}
 			w.WriteHeader(http.StatusOK)
 			json.NewEncoder(w).Encode(data)
-
+		} else if r.Method == http.MethodDelete {
+			name := r.URL.Path[1:]
+			if err := model.DeleteTodo(name); err != nil {
+				w.Write([]byte("Something went wrong"))
+				return
+			}
+			w.WriteHeader(http.StatusOK)
+			json.NewEncoder(w).Encode(struct {
+				Status string
+			}{"Item deleted"})
 		}
 	}
 }
